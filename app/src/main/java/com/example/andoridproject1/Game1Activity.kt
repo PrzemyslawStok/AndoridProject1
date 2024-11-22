70b33f5321f06255e4feb3305ed2f080e3728a6d
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2024-11-22 16:42:32 +0100

    menu activit added

--- app/src/main/java/com/example/andoridproject1/Game1Activity.kt
+++ app/src/main/java/com/example/andoridproject1/Game1Activity.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -22,16 +19,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.andoridproject1.ui.theme.AndoridProject1Theme
 import kotlin.random.Random
 
-class MainActivity : ComponentActivity() {
+class Game1Activity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -66,6 +61,7 @@ fun MyView(user: User, modifier: Modifier = Modifier) {
 
     fun endLevel(){
         noTrials = 0
+
     }
 
     fun nextTrial(){
---
+++ app/src/main/java/com/example/andoridproject1/MenuActivity.kt
@@ -0,0 +1,58 @@
+package com.example.andoridproject1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.andoridproject1.ui.theme.AndoridProject1Theme
+
+class MenuActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndoridProject1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MenuView(
+                        User("Przemysław", "Stokłosa", "Bielsko-Biała", "Wiśniowa", "43-300"),
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MenuView(user: User, modifier: Modifier = Modifier) {
+    Column(modifier = Modifier.padding(20.dp)) {
+        Row {
+            Text(
+                text = "next view",
+                fontSize = 50.sp
+            )
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MenuPreview() {
+    AndoridProject1Theme {
+        MyView(User("Przemysław", "Stokłosa", "Bielsko-Biała", "Wiśniowa", "43-300"))
+    }
+}
+
