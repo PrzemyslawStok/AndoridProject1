0410711e9b0cfd7ff004ba29def6a13119abcbe9
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2025-01-31 16:40:46 +0100

    dodano game3 ativity

--- app/src/main/java/com/example/andoridproject1/GameBoard.kt
+++ app/src/main/java/com/example/andoridproject1/GameBoard.kt
@@ -3,10 +3,16 @@ package com.example.andoridproject1
 import android.util.Log
 
 class GameBoard(val rows: Int, val cols: Int) {
-    val TAG = "tag"
+    private val tag = "tag"
+    private val gameBoard = IntArray(cols)
+
+    init {
+        for (i in 0..<cols) {
+            gameBoard[i] = 5
+        }
+    }
+
     fun array() {
-        val numbersArray = arrayOf(1, 2, 3, 4, 5)
-        Log.v(TAG, "${numbersArray.asList()}")
-        println(numbersArray)
+        Log.v(tag, "${gameBoard.asList()}")
     }
 }
